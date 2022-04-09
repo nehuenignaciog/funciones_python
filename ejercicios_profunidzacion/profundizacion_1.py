15a8a7d33f22a14af8c6d1210b4a71e4cfb3f97f
nehuenignaciog <nehuenignaciog@gmail.com>
2022-04-08 21:19:21 -0300

    Pendiente de Revisión

--- ejercicios_profunidzacion/profundizacion_1.py
+++ ejercicios_profunidzacion/profundizacion_1.py
@@ -51,11 +51,20 @@ Finalmente dicha función debe retornar la lista de elementos random generados.
 # Aquí dentro definir la función lista_aleatoria
 
 
+def lista_aleatoria(inicio, fin, cantidad):
+
+    lista_numeros = []
+
+    for i in range(cantidad):
+        numero = random.randint(inicio, fin)
+        lista_numeros.append(numero)
+    return lista_numeros
+
+
 # --------------------------------
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
-    
     inicio = 0
     fin = 10
     cantidad = 5
@@ -64,9 +73,13 @@ if __name__ == '__main__':
 
     # mi_lista_aleatoria = lista_aleatoria(inicio, fin, cantidad)
 
+    mi_lista_aleatoria = lista_aleatoria(inicio, fin, cantidad)
+
     # Imprimir en pantalla "mi_lista_aleatoria" que tendrá
     # los valores retornado por la función lista_aleatoria:
 
     # print(mi_lista_aleatoria)
+
+    print(mi_lista_aleatoria)
 
     print("terminamos")
--- ejercicios_profunidzacion/profundizacion_2.py
+++ ejercicios_profunidzacion/profundizacion_2.py
@@ -38,19 +38,30 @@ en la lista pueden usar el método nativo de list "count"
 # ya elaborada en el ejercicio anterior
 
 
+def lista_aleatoria(inicio, fin, cantidad):
+
+    lista_numeros = []
+
+    for i in range(cantidad):
+        numero = random.randint(inicio, fin)
+        lista_numeros.append(numero)
+    return lista_numeros
+
 # --------------------------------
 
 # --------------------------------
 # Aquí dentro definir la función contar
 
 
+def contar(lista, numero_contar):
+    return lista.count(numero_contar)
+
 # --------------------------------
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
-    
-    inicio = 0
-    fin = 10
+    inicio = 1
+    fin = 6
     cantidad = 5
 
     # Alumno: Utilizar la función "lista_aleatoria"
@@ -59,15 +70,23 @@ if __name__ == '__main__':
 
     # lista_numeros = lista_aleatoria()
 
+    lista_numeros = lista_aleatoria(inicio, fin, cantidad)
+
+
     # Imprimir en pantalla "lista_numeros" que tendrá
     # los valores retornado por la función "lista_aleatoria":
 
     # print(lista_numeros)
 
+    print(lista_numeros)
+
     # Luego quiero averiguar cuantas veces se repite el numero 3
     # en la lista aleatoria creada
     # cantidad_tres = contar(lista_numeros, 3)
 
+    cantidad_tres = contar(lista_numeros, 3)
+
     # print(cantidad_tres)
 
+    print(cantidad_tres)
     print("terminamos")
